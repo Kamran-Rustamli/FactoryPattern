db893faccc700afd1a13a40c690fd5e1f9a2e158
Kamran-Rustamli <131196738+Kamran-Rustamli@users.noreply.github.com>
2023-04-26 00:34:27 +0400

    BBB-44:Created factory pattern

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ public class Main {
     public static void main(String[] args) {
         AnimalFactory animalFactory = new AnimalFactory();
 
-        // create a Dog
+
         Animal dog = animalFactory.createAnimal("dog");
         dog.makeSound();
 
-        // create a Cat
+
         Animal cat = animalFactory.createAnimal("cat");
         cat.makeSound();
+
     }
 }
