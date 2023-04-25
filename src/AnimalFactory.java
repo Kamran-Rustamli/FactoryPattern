07ec64753de326d4538e6ebbb79fc7993f7bab53
Kamran-Rustamli <131196738+Kamran-Rustamli@users.noreply.github.com>
2023-04-26 00:32:25 +0400

    Initial commit

---
+++ src/AnimalFactory.java
@@ -0,0 +1,11 @@
+public class AnimalFactory {
+    public static Animal createAnimal(String type) {
+        if (type.equalsIgnoreCase("dog")) {
+            return new Dog();
+        } else if (type.equalsIgnoreCase("cat")) {
+            return new Cat();
+        } else {
+            return null;
+        }
+    }
+}
